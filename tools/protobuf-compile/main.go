3132b43a0444a1432ed7ca461bfb87786d7c7e91
Liam Cervante <liam.cervante@hashicorp.com>
2025-05-14 08:53:45 +0200

    switch to MPL2.0 licence

--- tools/protobuf-compile/main.go
+++ tools/protobuf-compile/main.go
@@ -1,5 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MPL-2.0
 
 // protobuf-compile is a helper tool for running protoc against all of the
 // .proto files in this repository using specific versions of protoc and
